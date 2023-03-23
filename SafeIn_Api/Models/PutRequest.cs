49d8b6a4b0a946f5f972dcb66e4fb805d969add3
khrystyna-kolba <hrystyna621@gmail.com>
2023-03-23 14:28:04 +0200

    Put request
    -enabled edit of users
    -changed azure connection

--- SafeIn_Api/Models/PutRequest.cs
+++ SafeIn_Api/Models/PutRequest.cs
@@ -2,7 +2,7 @@
 
 namespace SafeIn_Api.Models
 {
-    public class RegisterRequest
+    public class PutRequest
     {
         [Required]
         [EmailAddress]
@@ -11,12 +11,11 @@ namespace SafeIn_Api.Models
         [Required]
         [StringLength(50, MinimumLength = 1)]
         public string UserName { get; set; }
-
+        [Required]
+        [StringLength(50, MinimumLength = 8)]
+        public string CurrentPassword { get; set; }
         [Required]
         [StringLength(50, MinimumLength = 8)]
         public string Password { get; set; }
-
-        [Compare("Password")]
-        public string ConfirmPassword { get; set; }
     }
 }
